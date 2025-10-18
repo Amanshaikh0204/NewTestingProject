7a1b53f934abdd0dec3e2b41880ef6d57d5961bf
Aman Shaikh <107180181+Amanshaikh0204@users.noreply.github.com>
2025-10-18 13:29:44 +0530

    Update UnitTest1.cs

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -13,7 +13,7 @@
             await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
 
             // create a locator
-            var getStarted = Page.Locator("text=Get Started");
+            //Second Comment
 
             // Expect an attribute "to be strictly equal" to the value.
             await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
